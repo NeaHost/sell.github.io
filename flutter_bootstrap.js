9403bf6969c399117ab04fff14269ee1cc0254ae
NeaHost <mineamonster@gmail.com>
2025-04-22 00:41:54 +0700

    fix slow

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3972404940"
+    serviceWorkerVersion: "1870846670"
   }
 });
